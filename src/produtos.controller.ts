5083401ec173a66436aa6fa901cb34b8a2133e1a
nathaliadutra15 <you@example.com>
2021-12-27 14:40:56 -0300

    Adicionado uma classe com propriedades e funções nas rotas

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,34 +1,42 @@
 //Definição das rotas
 
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from "./produto.model";
 
 @Controller('produtos') // Decorator que indica que esta classe é um controller. Receber a url como parâmetro.
 export class ProdutosController {
+
+    produtos: Produto[] = [
+        new Produto("LIV01", "Livro TDD e BDD na prática", 29.90),
+        new Produto("LIV02", "Livro iniciando com Flutter", 39.90),
+        new Produto("LIV03", "Inteligência artificial como serviço", 49.90)
+    ];
+
     @Get()
-    obterTodos(): string {
-        return 'Lista todos os produtos';
+    obterTodos(): Produto[] {
+        return this.produtos;
     }
 
     @Get(':id') //parâmetro será um ID
     obterUm(@Param() parametros): string {
-        return `Retorna os dados do produto ${parametros.id}`;
+        return this.produtos[`${parametros.id}`];
     }
 
     @Post()
-    cadastrar(@Body() produto): string {
-        console.log(produto);
+    cadastrar(@Body() produto: Produto) {
+        produto.id = 100;
+        this.produtos.push(produto);
         return 'Produto criado!';
     }
 
     @Put()
-    alterarProduto(@Body() produto): string {
-        console.log(produto);
-        return 'Produto atualizado!';
+    alterarProduto(@Body() produto: Produto): Produto{
+        return produto;
     }
 
     @Delete(':id')
-    apagarProduto(@Param() parametros): string {
-        return `Apaga o produto ${parametros.id}`;
+    apagarProduto(@Param() parametros) {
+        this.produtos.pop();
     }
 
 }
